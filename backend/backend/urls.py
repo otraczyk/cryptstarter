38aa59ceba555f4a555ceea955aa1447f2c0a46f
Artur Kozak <artur.kozak@students.mimuw.edu.pl>
2015-11-28 14:24:02 +0100

    Add django-facebook settings

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -18,4 +18,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^facebook/', include('django_facebook.urls')),
+    url(r'^accounts/', include('django_facebook.auth_urls')), #Don't add this line if you use django registration or userena for registration and auth.
 ]
+
